8ab5f26edb95500d5baaa6768e1869c8d3530f71
Koichi Miyamoto <m5813k@gmail.com>
2019-01-25 18:30:09 +0900

    cssで色づけをして、配色を変えた

--- src/components/BezierControlPoint.vue
+++ src/components/BezierControlPoint.vue
@@ -1,9 +1,9 @@
 <template>
   <g class="control-points">
     <g v-for=" bp in path.breakpoints" :key="bp.x + ',' + bp.y">
-      <circle :cx="bp.x"             :cy="bp.y"             r="7" stroke="black"/>
-      <circle :cx="bp.startHandle.x" :cy="bp.startHandle.y" r="5" stroke="blue"/>
-      <circle :cx="bp.endHandle.x"   :cy="bp.endHandle.y"   r="5" stroke="red"/>
+      <circle class="breakpoint"   :cx="bp.x"             :cy="bp.y"            />
+      <circle class="start-handle" :cx="bp.startHandle.x" :cy="bp.startHandle.y"/>
+      <circle class="end-handle"   :cx="bp.endHandle.x"   :cy="bp.endHandle.y"  />
       <line 
         :x1="bp.startHandle.x" :y1="bp.startHandle.y"
         :x2="bp.endHandle.x" :y2="bp.endHandle.y"
@@ -24,5 +24,15 @@ export default class BezierControlPoint extends Vue {
 <style lang="scss">
 .control-points circle {
   fill: none;
+
+  stroke: black;
+  r: 7;
+  &.start-handle {
+    r: 5;
+    stroke: red;
+  }
+  &.end-handle {
+    r: 5;
+  }
 }
 </style>
