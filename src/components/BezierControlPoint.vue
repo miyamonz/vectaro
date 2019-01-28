04f8539712f366fc70de78e92708eeb513820278
Koichi Miyamoto <m5813k@gmail.com>
2019-01-28 19:35:28 +0900

    control pointのcircleをv-forで

--- src/components/BezierControlPoint.vue
+++ src/components/BezierControlPoint.vue
@@ -1,9 +1,10 @@
 <template>
   <g class="control-points">
     <g v-for=" bp in path.breakpoints" :key="bp.x + ',' + bp.y">
-      <circle class="breakpoint"   :cx="bp.x"             :cy="bp.y"            />
-      <circle class="start-handle" :cx="bp.startHandle.x" :cy="bp.startHandle.y"/>
-      <circle class="end-handle"   :cx="bp.endHandle.x"   :cy="bp.endHandle.y"  />
+      <circle v-for="[key, obj] in Object.entries(attrsSet(bp))" 
+        @click="click(key, obj)"
+        :class="key" v-bind="{...obj}"
+      />
       <line 
         :x1="bp.startHandle.x" :y1="bp.startHandle.y"
         :x2="bp.endHandle.x" :y2="bp.endHandle.y"
@@ -19,19 +20,36 @@ import { Breakpoint, BezierPath } from "@/types.ts";
 @Component
 export default class BezierControlPoint extends Vue {
   @Prop() private path!: BezierPath;
+
+  public attrsSet(bp: Breakpoint) {
+    const xy2cxcy = ({ x, y }: any) => ({ cx: x, cy: y });
+    return {
+      breakpoint: xy2cxcy(bp),
+      startHandle: xy2cxcy(bp.startHandle),
+      endHandle: xy2cxcy(bp.endHandle)
+    };
+  }
+
+  public click(t: "breakpoint" | "startHandle" | "endHandle") {
+    console.log(this.path.name, t);
+  }
 }
 </script>
 <style lang="scss">
 .control-points circle {
+  pointer-events: auto;
   fill: none;
-
   stroke: black;
   r: 7;
-  &.start-handle {
+  &:hover {
+    r: 20;
+    stroke-width: 2;
+  }
+  &.startHandle {
     r: 5;
     stroke: red;
   }
-  &.end-handle {
+  &.endHandle {
     r: 5;
   }
 }
