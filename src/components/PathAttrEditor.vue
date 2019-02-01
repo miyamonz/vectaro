ac22f23405444f45d929bd171acfbb738337a1fe
Koichi Miyamoto <m5813k@gmail.com>
2019-02-01 16:56:48 +0900

    PathAttrEditorを見た目ちょい調整

--- src/components/PathAttrEditor.vue
+++ src/components/PathAttrEditor.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
-    <span>{{path.name}}</span>
-    <pre>
-    {{path.breakpoints.length}} points
-    <input type="text" v-model="path.stroke">
-    <input type="text" v-model="path.strokeWidth">
-    <input type="text" v-model="path.fill">
-    <input type="checkbox" v-model="path.close">
-    </pre>
+    <div class="path-name">{{path.name}}</div>
+    <br>
+
+    <span>
+      {{path.breakpoints.length}} points
+    </span>
+    <div class="attrs">
+      <span>stroke</span> 
+      <input type="text" v-model="path.stroke">
+
+      <span>stroke-width</span> 
+      <input type="text" v-model="path.strokeWidth">
+
+      <span>fill</span> 
+      <input type="text" v-model="path.fill">
+
+      <span>close</span> 
+      <input type="checkbox" v-model="path.close">
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -17,3 +28,24 @@ export default class MainView extends Vue {
   @Prop() public path!: BezierPath;
 }
 </script>
+<style lang="scss">
+.path-name {
+  width: 100%;
+  border: solid 1px;
+}
+.attrs {
+  display: flex;
+  flex-wrap: wrap;
+  * {
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  span {
+    width: 40%;
+  }
+  input {
+    width: 60%;
+  }
+}
+</style>
