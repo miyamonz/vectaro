dd452567e1a0945eae4f822d80aa8c6f1784c626
Koichi Miyamoto <m5813k@gmail.com>
2019-02-06 21:26:48 +0900

    その他を切り出し

---
+++ src/components/Misc.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="misc">
+    <input v-model="adding" type="checkbox" style="width: 50px; transform: scale(2)"><br>
+    <button @click="$store.commit('setViewbox', [0,0,500,500])">reset viewport</button><br>
+    <ButtonDownload />
+    <InputFile />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+import ButtonDownload from "@/components/ButtonDownload.vue";
+import InputFile from "@/components/InputFile.vue";
+@Component({
+  components: {
+    ButtonDownload,
+    InputFile
+  }
+})
+export default class extends Vue {
+  get adding() {
+    return this.$store.state.editState.addingBreakpoint;
+  }
+  set adding(b: boolean) {
+    this.$store.dispatch("updateEditState", b);
+  }
+}
+</script>
