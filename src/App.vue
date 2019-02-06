375ef37efb93b840bac6290ecc53e7b88181f3a9
Koichi Miyamoto <m5813k@gmail.com>
2019-02-06 21:20:02 +0900

    その他をsidemenuに

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template>
   <div id="app">
     <router-view/>
-      <input v-model="adding" type="checkbox" style="width: 50px; transform: scale(2)">
-      <button @click="$store.commit('setViewbox', [0,0,500,500])">reset viewport</button>
-      <ButtonDownload />
-      <InputFile />
   </div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import ButtonDownload from "@/components/ButtonDownload.vue";
-import InputFile from "@/components/InputFile.vue";
 
-@Component({ components: { ButtonDownload, InputFile } })
-export default class extends Vue {
-  get adding() {
-    return this.$store.state.editState.addingBreakpoint;
-  }
-  set adding(b: boolean) {
-    this.$store.dispatch("updateEditState", b);
-  }
-}
+export default class extends Vue {}
 </script>
 <style lang="scss">
 #app {
--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -2,6 +2,12 @@
   <div class="side-menu">
     <PathMenu v-if="$store.getters.currentPath === null" @hovering="hovering = $event" />
     <PathAttrEditor v-else :path="$store.getters.currentPath"/>
+    <div>
+      <input v-model="adding" type="checkbox" style="width: 50px; transform: scale(2)">
+      <button @click="$store.commit('setViewbox', [0,0,500,500])">reset viewport</button>
+      <ButtonDownload />
+      <InputFile />
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -9,13 +15,24 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 import PathMenu from "@/components/PathMenu.vue";
 import PathAttrEditor from "@/components/PathAttrEditor.vue";
 
+import ButtonDownload from "@/components/ButtonDownload.vue";
+import InputFile from "@/components/InputFile.vue";
 @Component({
   components: {
     PathMenu,
-    PathAttrEditor
+    PathAttrEditor,
+    ButtonDownload,
+    InputFile
   }
 })
-export default class extends Vue {}
+export default class extends Vue {
+  get adding() {
+    return this.$store.state.editState.addingBreakpoint;
+  }
+  set adding(b: boolean) {
+    this.$store.dispatch("updateEditState", b);
+  }
+}
 </script>
 <style lang="scss">
 .side-menu {
