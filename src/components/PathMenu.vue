c41e9fbd8170b54d4b809c65f9c7ddc95ae5989d
Koichi Miyamoto <m5813k@gmail.com>
2019-02-08 21:25:24 +0900

    pathをdraggableにした

--- src/components/PathMenu.vue
+++ src/components/PathMenu.vue
@@ -1,23 +1,37 @@
 <template>
   <div class="path-menu">
-    <div
-      v-for="path,i in $store.state.paths"
-      class="path-item" 
-      :class="{hovering: path.key === $store.state.editState.hoveringPathKey}"
-      :key="path.key"
-      @mouseenter="enter(path.key)"
-      @mouseleave="leave(path.key)"
-      @click="select(path.key)"
-    >
-      {{path.name}}
-    </div>
+    <Draggable v-model="dragArr">
+      <div
+        v-for="path,i in $store.state.paths"
+        class="path-item" 
+        :class="{hovering: path.key === $store.state.editState.hoveringPathKey}"
+        :key="path.key"
+        @mouseenter="enter(path.key)"
+        @mouseleave="leave(path.key)"
+        @click="select(path.key)"
+      >
+        {{path.name}}
+      </div>
+    </Draggable>
   </div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-@Component
+import Draggable from "vuedraggable";
+@Component({
+  components: {
+    Draggable
+  }
+})
 export default class extends Vue {
   private over: boolean = false;
+
+  get dragArr() {
+    return this.$store.state.paths;
+  }
+  set dragArr(arr: any[]) {
+    this.$store.commit("setPaths", arr);
+  }
   public enter(key: string) {
     this.over = true;
     this.$emit("hovering", key);
